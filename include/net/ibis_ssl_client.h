35128e2a92ae2be499674e968611e6efd30e64a9
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2024-06-01 23:25:52 +0900

    omniOdometryの移植

--- include/net/ibis_ssl_client.h
+++ include/net/ibis_ssl_client.h
@@ -33,6 +33,7 @@
 
 #include "robot.h"
 #include "robot_control.h"
+#include "ring_buffer.h"
 
 #include "ibis_robot_packet.hpp"
 
@@ -164,36 +165,114 @@ private slots:
               return;
           }
 
+          {  // AI command update
+              orion.ai_cmd.local_target_speed[0] = packet->VEL_LOCAL_SURGE;
+              orion.ai_cmd.local_target_speed[1] = packet->VEL_LOCAL_SWAY;
 
-          orion.ai_cmd.local_target_speed[0] = packet->VEL_LOCAL_SURGE;
-          orion.ai_cmd.local_target_speed[1] = packet->VEL_LOCAL_SWAY;
+              orion.ai_cmd.local_target_speed_scalar = sqrt(
+                      pow(packet->VEL_LOCAL_SURGE, 2.) + pow(packet->VEL_LOCAL_SWAY, 2.));
+              orion.ai_cmd.global_vision_theta = packet->VISION_GLOBAL_THETA;
+              orion.ai_cmd.target_theta = packet->TARGET_GLOBAL_THETA;
+              orion.ai_cmd.chip_en = packet->CHIP_ENABLE;
+              orion.ai_cmd.kick_power = packet->KICK_POWER;
+              orion.ai_cmd.dribble_power = packet->DRIBBLE_POWER;
 
-          orion.ai_cmd.local_target_speed_scalar = sqrt(pow(packet->VEL_LOCAL_SURGE, 2.) + pow(packet->VEL_LOCAL_SWAY, 2.));
-          orion.ai_cmd.global_vision_theta = packet->VISION_GLOBAL_THETA;
-          orion.ai_cmd.target_theta = packet->TARGET_GLOBAL_THETA;
-          orion.ai_cmd.chip_en = packet->CHIP_ENABLE;
-          orion.ai_cmd.kick_power = packet->KICK_POWER;
-          orion.ai_cmd.dribble_power = packet->DRIBBLE_POWER;
+              auto raw_packet = static_cast<crane::RobotCommandSerialized>(*packet);
+              orion.ai_cmd.allow_local_flags = raw_packet.data[static_cast<int>(crane::RobotCommandSerialized::Address::LOCAL_FLAGS)];
 
-          auto raw_packet = static_cast<crane::RobotCommandSerialized>(*packet);
-          orion.ai_cmd.allow_local_flags = raw_packet.data[static_cast<int>(crane::RobotCommandSerialized::Address::LOCAL_FLAGS)];
+              orion.integ.pre_global_target_position[0] = packet->TARGET_GLOBAL_X;
+              orion.integ.pre_global_target_position[1] = packet->TARGET_GLOBAL_Y;
 
-          orion.integ.pre_global_target_position[0] = packet->TARGET_GLOBAL_X;
-          orion.integ.pre_global_target_position[1] = packet->TARGET_GLOBAL_Y;
+              orion.ai_cmd.global_ball_position[0] = packet->BALL_GLOBAL_X;
+              orion.ai_cmd.global_ball_position[1] = packet->BALL_GLOBAL_Y;
+              orion.ai_cmd.global_robot_position[0] = packet->VISION_GLOBAL_X;
+              orion.ai_cmd.global_robot_position[1] = packet->VISION_GLOBAL_Y;
+              orion.ai_cmd.global_target_position[0] = packet->TARGET_GLOBAL_X;
+              orion.ai_cmd.global_target_position[1] = packet->TARGET_GLOBAL_Y;
 
-          orion.ai_cmd.global_ball_position[0] = packet->BALL_GLOBAL_X;
-          orion.ai_cmd.global_ball_position[1] = packet->BALL_GLOBAL_Y;
-          orion.ai_cmd.global_robot_position[0] = packet->VISION_GLOBAL_X;
-          orion.ai_cmd.global_robot_position[1] = packet->VISION_GLOBAL_Y;
-          orion.ai_cmd.global_target_position[0] = packet->TARGET_GLOBAL_X;
-          orion.ai_cmd.global_target_position[1] = packet->TARGET_GLOBAL_Y;
-
-          orion.ai_cmd.vision_lost_flag = ! packet->IS_ID_VISIBLE;
+              orion.ai_cmd.vision_lost_flag = !packet->IS_ID_VISIBLE;
 //        orion.ai_cmd.local_vision_en_flag = packet->LOCAL_VISION_EN;
-          orion.ai_cmd.local_vision_en_flag = false;
-          orion.ai_cmd.keeper_mode_en_flag = packet->LOCAL_KEEPER_MODE_ENABLE;
-          orion.ai_cmd.stop_request_flag = packet->STOP_FLAG;
-          orion.ai_cmd.dribbler_up_flag = packet->IS_DRIBBLER_UP;
+              orion.ai_cmd.local_vision_en_flag = false;
+              orion.ai_cmd.keeper_mode_en_flag = packet->LOCAL_KEEPER_MODE_ENABLE;
+              orion.ai_cmd.stop_request_flag = packet->STOP_FLAG;
+              orion.ai_cmd.dribbler_up_flag = packet->IS_DRIBBLER_UP;
+
+              orion.integ.vision_based_position[0] = packet->VISION_GLOBAL_X;
+              orion.integ.vision_based_position[1] = packet->VISION_GLOBAL_Y;
+          }
+
+          {  // Odom update
+              // https://github.com/ibis-ssl/G474_Orion_main/blob/main/Core/Src/odom.c#L32
+              for(int i = 0; i < 4; i++) {
+                  auto & wheel = _robot->wheels[i];
+                  if (isnan(orion.motor.enc_angle[i])) {
+                      orion.motor.enc_angle[i] = 0;
+                  }
+                  orion.motor.enc_angle[i] = static_cast<float>(dJointGetAMotorAngle(wheel->motor, 0));
+                  orion.motor.angle_diff[i] = orion.motor.enc_angle[i] - orion.motor.pre_enc_angle[i];
+                  orion.motor.pre_enc_angle[i] = orion.motor.enc_angle[i];
+              }
+
+              const double omni_diameter = [&](){
+                  dReal radius, length;
+                  dGeomCylinderGetParams(_robot->wheels[0]->cyl->geom, &radius, &length);
+                  return radius * 2.0;
+              }();
+              orion.omni.travel_distance[0] = orion.motor.angle_diff[1] * omni_diameter;
+              orion.omni.travel_distance[1] = orion.motor.angle_diff[2] * omni_diameter;
+
+              // right front & left front
+              orion.omni.odom_raw[0] += orion.omni.travel_distance[0] * cos(orion.imu.yaw_angle_rad) + orion.omni.travel_distance[1] * sin(orion.imu.yaw_angle_rad);
+              orion.omni.odom_raw[1] += orion.omni.travel_distance[0] * sin(orion.imu.yaw_angle_rad) - orion.omni.travel_distance[1] * cos(orion.imu.yaw_angle_rad);
+
+              orion.omni.pre_odom[0] = orion.omni.odom[0];
+              orion.omni.pre_odom[1] = orion.omni.odom[1];
+
+              orion.omni.odom[0] = ((orion.omni.odom_raw[0] * cos(M_PI * 3 / 4) - orion.omni.odom_raw[1] * sin(M_PI * 3 / 4)) / 2) + (0.107 * cos(orion.imu.yaw_angle_rad) - 0.107);
+              orion.omni.odom[1] = ((orion.omni.odom_raw[0] * sin(M_PI * 3 / 4) + orion.omni.odom_raw[1] * cos(M_PI * 3 / 4)) / 2) + (0.107 * sin(orion.imu.yaw_angle_rad));
+
+              //   omni->odom_speed[0] = (omni->odom[0] - omni->pre_odom[0]) * MAIN_LOOP_CYCLE;
+              //  omni->odom_speed[1] = (omni->odom[1] - omni->pre_odom[1]) * MAIN_LOOP_CYCLE;
+              orion.omni.odom_speed[0] = (orion.omni.odom[0] - orion.omni.pre_odom[0]) / 0.01;
+              orion.omni.odom_speed[1] = (orion.omni.odom[1] - orion.omni.pre_odom[1]) / 0.01;
+
+              // omni->local_odom_speed[0] = omni->odom_speed[0] * cos(-imu->yaw_angle_rad) - omni->odom_speed[1] * sin(-imu->yaw_angle_rad);
+              // omni->local_odom_speed[1] = omni->odom_speed[0] * sin(-imu->yaw_angle_rad) + omni->odom_speed[1] * cos(-imu->yaw_angle_rad);
+              orion.omni.local_odom_speed[0] = orion.omni.odom_speed[0] * cos(-orion.imu.yaw_angle_rad) - orion.omni.odom_speed[1] * sin(-orion.imu.yaw_angle_rad);
+              orion.omni.local_odom_speed[1] = orion.omni.odom_speed[0] * sin(-orion.imu.yaw_angle_rad) + orion.omni.odom_speed[1] * cos(-orion.imu.yaw_angle_rad);
+
+              // for (int i = 0; i < 2; i++) {
+              //    enqueue(omni->local_speed_log[i], omni->local_odom_speed[i]);
+              //    omni->local_odom_speed_mvf[i] = sumNewestN(omni->local_speed_log[i], SPEED_MOVING_AVERAGE_FILTER_BUF_SIZE) / SPEED_MOVING_AVERAGE_FILTER_BUF_SIZE;
+              //  }
+              for (int i = 0; i <2 ; ++i) {
+                enqueue(orion.omni.local_speed_log[i], orion.omni.local_odom_speed[i]);
+                orion.omni.local_odom_speed_mvf[i] = sumNewestN(orion.omni.local_speed_log[i], SPEED_MOVING_AVERAGE_FILTER_BUF_SIZE) / SPEED_MOVING_AVERAGE_FILTER_BUF_SIZE;
+              }
+
+              // local座標系で入れているodom speedを,global系に修正する
+              // vision座標だけ更新されているが、vision_update_cycle_cntが0になっていない場合に、1cycleだけpositionが飛ぶ
+              float latency_cycle = orion.ai_cmd.latency_time_ms / (1000 / MAIN_LOOP_CYCLE);
+              for (int i = 0; i < 2; i++) {
+                enqueue(orion.integ.odom_log[i], orion.omni.odom_speed[i]);
+                // メモ：connection.vision_update_cycle_cntは更新できていない
+                orion.integ.global_odom_vision_diff[i] = sumNewestN(orion.integ.odom_log[i], latency_cycle + orion.connection.vision_update_cycle_cnt) / MAIN_LOOP_CYCLE;
+                orion.integ.vision_based_position[i] = orion.ai_cmd.global_robot_position[i] + orion.integ.global_odom_vision_diff[i];
+                orion.integ.position_diff[i] = orion.ai_cmd.global_target_position[i] - orion.integ.vision_based_position[i];
+              }
+              float target_diff[2], move_diff[2];
+              for (int i = 0; i < 2; i++) {
+                target_diff[i] = orion.ai_cmd.global_robot_position[i] - orion.ai_cmd.global_target_position[i];  // Visionが更新された時点での現在地とtargetの距離
+                move_diff[i] = orion.ai_cmd.global_robot_position[i] - orion.integ.vision_based_position[i];      // Visionとtargetが更新されてからの移動量
+              }
+
+              orion.integ.target_dist_diff = sqrt(pow(target_diff[0], 2) + pow(target_diff[1], 2));
+              orion.integ.move_dist = sqrt(pow(orion.integ.position_diff[0], 2) + pow(orion.integ.position_diff[1], 2));
+          }
+
+          dReal x,y;
+          _robot->getXY(x, y);
+//          orion.omni.robot_position[0] = x;
           const double last_dt = 0.01;
           double omega = getOmega(
                   _robot->getDir() * M_PI / 180.0, packet->TARGET_GLOBAL_THETA, last_dt);
@@ -244,6 +323,8 @@ public:
         output_t output;
         omni_t omni;
         debug_t debug;
+        motor_t motor;
+        connection_t connection;
   } orion;
 
   //  QNetworkInterface *_net_interface;
